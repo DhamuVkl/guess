3c85ec5dbedbf1a4d75fd0601831d04545d58256
Dharagesh <sdhamuvkl@gmail.com>
2024-04-11 09:23:33 +0530

    #

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::io;
-
+//use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    //println!("Please input your guess.");
+    
+    let secret_number = rand::thread_rng().gen_range(1, 1001);
+    println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
+    //let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
-
+    //io::stdin()
+       // .read_line(&mut guess)
+       // .expect("Failed to read line");
+    //println!("You guessed: {}", guess);
 }
